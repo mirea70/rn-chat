50350e2bf5389747275a25800e41912e54c0677d
mirea70 <deae670@daum.net>
2024-01-16 07:46:19 +0900

    feat: 로그인 화면 추가 (기능 X)

--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext, useState, useRef } from 'react';
+import { ThemeContext } from 'styled-components/native';
 import styled from 'styled-components/native';
-import { Button } from 'react-native';
+import { Button, Image, Input } from '../components';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { theme } from '../theme';
 
 const Container = styled.View`
   flex: 1;
@@ -13,17 +15,49 @@ const Container = styled.View`
   padding-bottom: ${({ insets: { bottom } }) => bottom}px;
 `;
 
-const StyledText = styled.Text`
-  font-size: 30px;
-  color: #111111;
-`;
+const LOGO =
+  'https://firebasestorage.googleapis.com/v0/b/rn-chat-a8a6b.appspot.com/o/logo.png?alt=media';
+
 const Signin = ({ navigation }) => {
   const insets = useSafeAreaInsets();
+  const theme = useContext(ThemeContext);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const refPassword = useRef(null);
+
+  const _handleSigninBtnPress = () => {
+    console.log('signin');
+  };
 
   return (
     <Container insets={insets}>
-      <StyledText>Signin</StyledText>
-      <Button title="signup" onPress={() => navigation.navigate('Signup')} />
+      <Image url={LOGO} />
+      <Input
+        label="Email"
+        placeholder="Email"
+        returnKeyType="next"
+        value={email}
+        onChangeText={setEmail}
+        onSubmitEditing={() => refPassword.current.focus()}
+      />
+      <Input
+        ref={refPassword}
+        label="Password"
+        placeholder="Password"
+        returnKeyType="done"
+        value={password}
+        onChangeText={setPassword}
+        isPassword={true}
+        onSubmitEditing={_handleSigninBtnPress}
+      />
+      <Button title="Sign in" onPress={_handleSigninBtnPress} />
+      <Button
+        title="or sign up"
+        onPress={() => navigation.navigate('Signup')}
+        containerStyle={{ marginTop: 0, backgroundColor: 'transparent' }}
+        textStyle={{ color: theme.btnTextLink, fontSize: 18 }}
+      />
     </Container>
   );
 };
--- src/theme.js
+++ src/theme.js
@@ -2,9 +2,25 @@ const Colors = {
   white: '#ffffff',
   black: '#111111',
   main: '#3679fe',
+  grey_0: '#d5d5d5',
+  grey_1: '#a6a6a6',
 };
 
 export const theme = {
   background: Colors.white,
-  text: Colors.text,
+  text: Colors.black,
+
+  // Button
+  btnBackground: Colors.main,
+  btnTitle: Colors.white,
+  btnTextLink: Colors.main,
+
+  // Image
+  imgBackground: Colors.grey_0,
+
+  // Input
+  inputBackground: Colors.white,
+  inputLabel: Colors.grey_1,
+  inputPlaceholder: Colors.grey_1,
+  inputBorder: Colors.grey_1,
 };
